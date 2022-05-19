5ee4670f396b274e7be109fc9777293d10b9f6f4
Laxmi16101995 <Laxmiwavale1995@gmail.com>
2022-05-19 19:04:31 +0530

    Day 1

---
+++ Details_Print.java
@@ -0,0 +1,14 @@
+
+public class Details_Print {
+	public static void main(String[] args) {
+		System.out.println("Name : Laxmi Wavale");
+		System.out.println("Father’s Name : Munjaji Wavale");
+		System.out.println("Mother ‘s Name : Shakuntala Wavale");
+		System.out.println("Age : 26");
+		System.out.println("Gender : Female");
+		System.out.println("Address : Nanded, Maharastra");
+		System.out.println("Mobile No. : 8208038245");
+
+	}
+
+}
