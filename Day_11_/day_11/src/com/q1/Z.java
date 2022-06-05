34eca737ca49eb2f8041fec3db85e28b0aa3bccc
Laxmi16101995 <100854607+Laxmi16101995@users.noreply.github.com>
2022-06-05 20:18:24 +0530

    Update Z.java

--- Day_11_/day_11/src/com/q1/Z.java
+++ Day_11_/day_11/src/com/q1/Z.java
@@ -3,5 +3,11 @@ package com.q1;
 public interface Z extends X,Y{
 	
 	void abstractMethodZ();
+	
+	@Override
+	default void defaultMethodX() {
+		System.out.println("inside defaultMethodX of z interface");
+
+	}
 
 }
